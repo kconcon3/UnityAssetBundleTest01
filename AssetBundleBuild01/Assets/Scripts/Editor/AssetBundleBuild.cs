3f410db2c3f0c9d3f97dfad003a5dccd448391a6
kconcon3 <hkane3sub@excite.co.jp>
2018-10-12 16:42:21 +0900

    Custom Packageではなく、Assetsの下をコミットするように変更

---
+++ AssetBundleBuild01/Assets/Scripts/Editor/AssetBundleBuild.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class AssetBundleBuild {
+
+	[MenuItem("Expansion/Build AssetBundleData")]
+	public static void Build()
+	{
+		string assetBundleDirectory = "./AssetBundleData";		// 出力先ディレクトリ
+
+		// 出力先ディレクトリが無かったら作る
+		if( !Directory.Exists(assetBundleDirectory) )
+		{
+			Directory.CreateDirectory(assetBundleDirectory);
+		}
+
+		// AssetBundleのビルド(ターゲット(プラットフォーム)毎に3つ目の引数が違うので注意)
+		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+
+		// ビルド終了を表示
+		EditorUtility.DisplayDialog("アセットバンドルビルド終了", "アセットバンドルビルドが終わりました", "OK");		
+	}
+}
+
---
+++ AssetBundleLoad01/Assets/Scripts/TestLoad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLoad : MonoBehaviour {
+
+	void Start ()
+	{
+		// アセットバンドルのロード
+		AssetBundle ab = AssetBundle.LoadFromFile("AssetBundleData/asset_pack01");
+
+		if( ab == null )
+		{
+			// ロード失敗
+			Debug.Log("error : load bundle");
+		}
+		else
+		{
+			// ロード成功
+			Debug.Log("success : load bundle");
+
+			// Prefabのロード
+			GameObject prefab = ab.LoadAsset<GameObject>("Cat01");		// 注：LoadAssetは階層指定が出来ない
+
+			if( prefab == null )
+			{
+				// Prefabのロード失敗
+				Debug.Log("error : load asset");
+			}
+			else
+			{
+				// Prefabのロード成功
+
+				// Prefabをインスタンス化
+				Instantiate(prefab);
+			}
+		}
+	}
+}
